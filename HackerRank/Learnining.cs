4d19422600a6ab8e2bf52c3e0e48a4d6c314a86d
PrithiviRaj <Prithivirajp.cvr@gmail.com>
2021-02-22 22:57:40 +0530

    New Commit training

---
+++ HackerRank/Learnining.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Learnining
+    {
+    }
+
+    public static class Calculator
+    {
+        private  int _resultStorage = 0;
+
+        public  string Type = "Arithmetic";
+
+        public  int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public  void Store(int result)
+        {
+            _resultStorage = result;
+        }
+    }
+}
